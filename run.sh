4c4f20a707b6b6cfe32b5b11e68e86787ce7b7ef
Matthew Maggio <me@techwizmatt.info>
2022-03-01 18:01:06 -0700

    Update

--- run.sh
+++ run.sh
@@ -4,5 +4,17 @@ if [[ $USER != 'root' ]]; then
   echo 'Please run as root, Goodbye'
   exit
 fi
-  echo ':: Building projects point on "/data" ::'
-  
+  echo ':: Generating groups, directories, and installing packages ::'
+  echo 'You will need to add the group "www-iot" to your user'
+  groupadd www-iot
+  cd /
+  mkdir data
+  chgrp -R www-iot data/
+  cd /data
+  apt install node -y
+  apt install npm -y
+  node -v
+  npm -v
+  git clone https://github.com/Techwizmatt/DynamicDNS-Godaddy.git
+  cd DynamicDNS-Godaddy
+  npm i
